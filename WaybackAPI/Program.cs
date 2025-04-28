0883d14e4d4a46d386910300234c8441e367c5f5
Justin Kruskie <justin@kruskie.net>
2025-04-28 19:03:45 -0400

    Finish some comments

--- WaybackAPI/Program.cs
+++ WaybackAPI/Program.cs
@@ -1,36 +1,44 @@
 using Microsoft.OpenApi.Models;
-using WaybackAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http.Json;
 using System.Text.Json.Serialization;
-using Microsoft.EntityFrameworkCore;
+using WaybackAPI.Models;
 using WaybackAPI.Routes;
 using WaybackAPI.Interfaces;
 using WaybackAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
+// ===== Services =====
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: "AllowAllOrigins",
-        configurePolicy: policy =>
-        {
-            policy.AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
+
 builder.Services.Configure<JsonOptions>(options =>
 {
     options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
+
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     });
-builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddDbContext<WaybackAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("KruskieLab")));
+
+builder.Services.AddScoped<IGameService, GameService>();
+builder.Services.AddScoped<IContributorService, ContributorService>();
+builder.Services.AddScoped<IMediaService, MediaService>();
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -41,15 +49,9 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-builder.Services.AddDbContext<WaybackAPIContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("KruskieLab")));
-
-builder.Services.AddScoped<IGameService, GameService>();
-builder.Services.AddScoped<IContributorService, ContributorService>();
-builder.Services.AddScoped<IMediaService, MediaService>();
-
 var app = builder.Build();
 
+// ===== Middleware =====
 app.UseCors("AllowAllOrigins");
 
 app.UseSwagger();
@@ -58,6 +60,7 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "BAISD - Wayback Machine v1");
 });
 
+// ===== Routes =====
 app.MapGet("/", () => "Bay-Arenac ISD Career Center - Computer Programming - Wayback Machine API");
 
 app.MapGameRoutes();
