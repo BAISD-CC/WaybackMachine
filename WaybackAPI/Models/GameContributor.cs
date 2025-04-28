f3a1eeb978ebc4b1df17d14fd94d22c844e146be
Justin Kruskie <justin@kruskie.net>
2025-04-28 17:19:39 -0400

    Make all attributes in GameContributor.cs required

--- WaybackAPI/Models/GameContributor.cs
+++ WaybackAPI/Models/GameContributor.cs
@@ -3,17 +3,15 @@ using System.ComponentModel.DataAnnotations;
 namespace WaybackAPI.Models
 {
 
-   public class GameContributor
-   {
-
-      [Required]
-      public required int GameId { get; set; }
-      [Required]
-      public required int ContributorId { get; set; }
-
-      public required Game Game { get; set; }
-      public required Contributor Contributor { get; set; }
-
-   }
-
+    public class GameContributor
+    {
+        [Required]
+        public required int GameId { get; set; }
+        [Required]
+        public required Game Game { get; set; }
+        [Required]
+        public required int ContributorId { get; set; }
+        [Required]
+        public required Contributor Contributor { get; set; }
+    }
 }
